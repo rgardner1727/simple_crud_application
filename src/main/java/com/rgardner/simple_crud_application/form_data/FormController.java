4458420bac75a795ee710ace1091155144085a36
Ryan Gardner <rgardner1727@gmail.com>
2024-11-24 21:00:03 -0500

    Broke separated logout on nav somehow, but nav and footer are now added using a fragments file

--- src/main/java/com/rgardner/simple_crud_application/form_data/FormController.java
+++ src/main/java/com/rgardner/simple_crud_application/form_data/FormController.java
@@ -1,19 +1,11 @@
 package com.rgardner.simple_crud_application.form_data;
 
-import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
